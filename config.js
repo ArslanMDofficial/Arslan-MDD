801bf46a165089506a10540204dc8e5778844bd5
Arslan-MD <157588592+ArslanMDofficial@users.noreply.github.com>
2025-01-21 09:33:17 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "enter your session",
+    SESSION_ID: process.env.SESSION_ID || "sTkHmQZI#cti2Mzwizp4Ib9g5Yi1PfEvcGNiYd4KDQWY78cG-6rE",
     SUDO_NB: process.env.SUDO_NB || "923237045919",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
     READ_MESSAGE: process.env.READ_MESSAGE || "false", // Added auto-read configuration
